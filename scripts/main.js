5704861bda791f135d31faad0974c6477c674afe
Endersson Alonso Mendoza Muñoz <enderalonso24@gmail.com>
2021-09-02 17:44:58 -0600

    Terminado sitio escritorio, comienzo responsive

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.contenedor-items', { delay: 200 });
+ScrollReveal().reveal('.information-cards', { delay: 200 });
+ScrollReveal().reveal('.elegir-container', { delay: 200 });
+ScrollReveal().reveal('.servicios-ofrecidos', { delay: 200 });
+ScrollReveal().reveal('.proyectos', { delay: 650 });
+ScrollReveal().reveal('.formulario-contacto', { delay: 200 });
+ScrollReveal().reveal('.footer-links', { delay: 200 });
+ScrollReveal().reveal('.lista-eleccion', { delay: 700 });
+
