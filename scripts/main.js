626b6be5cedb795da24e51109ea0295a0df95540
Endersson Alonso Mendoza Muñoz <enderalonso24@gmail.com>
2021-09-04 13:49:14 -0600

    Gran avance en el modo responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,54 @@
 ScrollReveal().reveal('.showcase');
-// ScrollReveal().reveal('.contenedor-items', { delay: 200 });
-ScrollReveal().reveal('.information-cards', { delay: 200 });
-ScrollReveal().reveal('.elegir-container', { delay: 200 });
-ScrollReveal().reveal('.servicios-ofrecidos', { delay: 200 });
-ScrollReveal().reveal('.proyectos', { delay: 650 });
-ScrollReveal().reveal('.formulario-contacto', { delay: 200 });
-ScrollReveal().reveal('.footer-links', { delay: 200 });
-ScrollReveal().reveal('.lista-eleccion', { delay: 700 });
+// // ScrollReveal().reveal('.contenedor-items', { delay: 200 });
+// ScrollReveal().reveal('.information-cards', { delay: 200 });
+// ScrollReveal().reveal('.elegir-container', { delay: 200 });
+// ScrollReveal().reveal('.servicios-ofrecidos', { delay: 200 });
+// ScrollReveal().reveal('.proyectos', { delay: 650 });
+// ScrollReveal().reveal('.formulario-contacto', { delay: 200 });
+// ScrollReveal().reveal('.footer-links', { delay: 200 });
+// ScrollReveal().reveal('.lista-eleccion', { delay: 700 });
 
 
 window.sr = ScrollReveal();
 
 sr.reveal(".contenedor-items", {
-    duration: 3000,
+    duration: 4000,
     origin: 'left',
-    distance: '-100px'
+    distance: '-1000px'
 })
 
-sr.reveal(".card-one-services", {
-    origin: 'right',
-    interval: 200,
-    duration: 5000,
-    distance: '150%'
-})
-sr.reveal(".card-two-services", {
-    origin: '',
-    interval: 200,
-    duration: 5000,
-    distance: '150%'
-})
 sr.reveal(".cards", {
-    origin: 'top',
-    interval: 200,
-    duration: 5000,
-    distance: '150%'
+    origin: 'right',
+    interval: 400,
+    duration: 3000,
+    distance: '-1000px'
 })
 
+
+sr.reveal(".elegir-container", {
+    origin: 'left',
+    interval: 400,
+    duration: 3000,
+    distance: '-1000px'
+})
+
+sr.reveal(".servicios-ofrecidos", {
+    origin: 'right',
+    interval: 400,
+    duration: 3000,
+    distance: '-1000px'
+});
+
+sr.reveal(".proyectos", {
+    origin: 'top',
+    interval: 900,
+    duration: 3000,
+    distance: '-1000px'
+});
+
+sr.reveal(".formulario-contacto", {
+    origin: 'bottom',
+    interval: 400,
+    duration: 3000,
+    distance: '1000px'
+})
